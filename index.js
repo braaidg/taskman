c7f5802e4b78eb9f53277dd644df581928df4419
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-11 16:37:09 -0300

    Initialized express server. Added mongo DB connection

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import dbConnection from "./config/db.js";
+
+const app = express();
+
+dotenv.config();
+
+dbConnection();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
