d853fc4dfa95967b456831ba2459e4c69e2209b0
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-11 17:52:45 -0300

    register now check for existing user email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
-  const bodyData = req.body;
+  const { email } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    const error = new Error("User already registered");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
-    const user = new User(bodyData);
+    const user = new User(req.body);
     const userOnDb = await user.save();
     res.json(userOnDb);
   } catch (error) {
