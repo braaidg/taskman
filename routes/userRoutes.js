56a17c166b91d467782f57dfdd9e962d0587adae
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-11 22:17:03 -0300

    Added forgot password routes and controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,20 @@ import {
   register,
   authenticate,
   confirmAccount,
+  forgotPassword,
+  checkToken,
+  newPassword,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", register);
 router.post("/login", authenticate);
+
+router.post("/forgot-password", forgotPassword);
+
 router.get("/confirm/:token", confirmAccount);
+
+router.route("/forgot-password/:token").get(checkToken).post(newPassword);
 
 export default router;
