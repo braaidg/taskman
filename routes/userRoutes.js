371e37db0ac1c88d50699c06ba68ddffedce80dc
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-11 21:48:28 -0300

    Added /confirm/:token route to use confirmAccount

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { register, authenticate } from "../controllers/userController.js";
+import {
+  register,
+  authenticate,
+  confirmAccount,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", register);
 router.post("/login", authenticate);
+router.get("/confirm/:token", confirmAccount);
 
 export default router;
