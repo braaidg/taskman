5a00e0fc69e294e7e743eec6c3448df996f84af9
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-18 00:04:45 -0300

    Created project info update via PUT action

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -38,7 +38,33 @@ const getProject = async (req, res) => {
   }
 };
 
-const editProject = async (req, res) => {};
+const editProject = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const projectFromDb = await Project.findById(id);
+
+    if (!projectFromDb) {
+      const error = new Error("Project not found");
+      return res.status(404).json({ msg: error.message });
+    }
+
+    if (projectFromDb.creator.toString() !== req.user._id.toString()) {
+      const error = new Error("Action not valid");
+      return res.status(401).json({ msg: error.message });
+    }
+
+    projectFromDb.name = req.body.name || projectFromDb.name;
+    projectFromDb.description =
+      req.body.description || projectFromDb.description;
+    projectFromDb.finishDate = req.body.finishDate || projectFromDb.finishDate;
+    projectFromDb.client = req.body.client || projectFromDb.client;
+
+    const projectUpdated = await projectFromDb.save();
+    return res.json(projectUpdated);
+  } catch (error) {
+    return res.status(400).json({ msg: "Action not valid" });
+  }
+};
 
 const deleteProject = async (req, res) => {};
 
