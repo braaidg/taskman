a9c05a6f23cc1207893ceb722753f3c80d1fb6c1
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-17 22:13:49 -0300

    Added projects controller functions

---
+++ controllers/projectController.js
@@ -0,0 +1,26 @@
+const getProjects = async (req, res) => {};
+
+const newProject = async (req, res) => {};
+
+const getProject = async (req, res) => {};
+
+const editProject = async (req, res) => {};
+
+const deleteProject = async (req, res) => {};
+
+const addCollaborator = async (req, res) => {};
+
+const deleteCollaborator = async (req, res) => {};
+
+const getTasks = async (req, res) => {};
+
+export {
+  getProject,
+  addCollaborator,
+  deleteCollaborator,
+  deleteProject,
+  editProject,
+  getProjects,
+  getTasks,
+  newProject,
+};
