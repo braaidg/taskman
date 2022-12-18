6951d67bf743d4a6a5ec12271deae4741cf6147d
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-17 22:44:38 -0300

    add: New project creation functionality

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,18 @@
+import Project from "../models/Project.js";
+
 const getProjects = async (req, res) => {};
 
-const newProject = async (req, res) => {};
+const newProject = async (req, res) => {
+  const project = new Project(req.body);
+  project.creator = req.user._id;
+
+  try {
+    const projectOnDb = await project.save();
+    res.json(projectOnDb);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getProject = async (req, res) => {};
 
