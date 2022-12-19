98eee4a620fc9bfa494dcce1c1c9c64921a8e4a2
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-19 17:33:43 -0300

    Added task related routes to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import dbConnection from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -27,6 +28,7 @@ app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 4000;
 
