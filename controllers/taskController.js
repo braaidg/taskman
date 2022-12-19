77a94c930f544035a01cee3bc484d6da480e2cc6
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-19 17:43:56 -0300

    Added functionallity to addTask function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,29 @@
-const addTask = async (req, res) => {};
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
+
+const addTask = async (req, res) => {
+  const { project } = req.body;
+
+  const projectOnDb = await Project.findById(project);
+
+  if (!projectOnDb) {
+    const error = new Error("Project not found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (projectOnDb.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("You dont have permission to add tasks");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    const taskOnDb = await Task.create(req.body);
+    return res.json(taskOnDb);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getTask = async (req, res) => {};
 const updateTask = async (req, res) => {};
 const deleteTask = async (req, res) => {};
