66caf8447a7c2dfa59aed851b11638107d2e3bc8
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-19 17:34:10 -0300

    Created controllers for task routes

---
+++ controllers/taskController.js
@@ -0,0 +1,7 @@
+const addTask = async (req, res) => {};
+const getTask = async (req, res) => {};
+const updateTask = async (req, res) => {};
+const deleteTask = async (req, res) => {};
+const changeTaskState = async (req, res) => {};
+
+export { addTask, getTask, updateTask, deleteTask, changeTaskState };
